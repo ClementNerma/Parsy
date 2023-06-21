3e4f3d0403ab586542153c64849e9aba4156e213
Clément Nerma <clement.nerma@gmail.com>
2023-06-21 10:50:58 +0200

    Remove Clone constraint on Custom

--- src/parsers/simples/custom.rs
+++ src/parsers/simples/custom.rs
@@ -1,17 +1,17 @@
 use crate::{PResult, Parser, ParserInput};
 
 #[derive(Clone)]
-pub struct Custom<F: Fn(&mut ParserInput) -> PResult<O> + Clone, O> {
+pub struct Custom<F: Fn(&mut ParserInput) -> PResult<O>, O> {
     func: F,
 }
 
-impl<F: Fn(&mut ParserInput) -> PResult<O> + Clone, O> Custom<F, O> {
+impl<F: Fn(&mut ParserInput) -> PResult<O>, O> Custom<F, O> {
     pub fn new(func: F) -> Self {
         Self { func }
     }
 }
 
-impl<F: Fn(&mut ParserInput) -> PResult<O> + Clone, O> Parser<O> for Custom<F, O> {
+impl<F: Fn(&mut ParserInput) -> PResult<O>, O> Parser<O> for Custom<F, O> {
     fn parse_inner(&self, input: &mut ParserInput) -> PResult<O> {
         (self.func)(input)
     }
