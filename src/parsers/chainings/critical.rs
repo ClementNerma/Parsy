af4dc7f8747913112ca53b0643aa3c6829f2f61e
Clément Nerma <clement.nerma@gmail.com>
2023-12-07 22:24:23 +0100

    Improve 'critical' parser

--- src/parsers/chainings/critical.rs
+++ src/parsers/chainings/critical.rs
@@ -8,6 +8,7 @@ use crate::{PResult, Parser, ParserInput};
 pub struct Critical<T, P: Parser<T>> {
     parser: P,
     message: Option<&'static str>,
+    unexpected_eof_msg: bool,
     _t: PhantomData<T>,
 }
 
@@ -16,8 +17,14 @@ impl<T, P: Parser<T>> Critical<T, P> {
         Self {
             parser,
             message,
+            unexpected_eof_msg: true,
             _t: PhantomData,
         }
+    }
+
+    pub fn unexpected_eof_msg(mut self, enable: bool) -> Self {
+        self.unexpected_eof_msg = enable;
+        self
     }
 }
 
@@ -26,18 +33,16 @@ impl<T, P: Parser<T>> Parser<T> for Critical<T, P> {
         let is_empty = input.inner().is_empty();
 
         self.parser.parse(input).map_err(|err| {
-            let message = match self.message {
-                Some(message) => Cow::Borrowed(message),
-                None => match err.atomic_error() {
+            let message = if is_empty && self.unexpected_eof_msg {
+                Cow::Borrowed("unexpected end of input")
+            } else {
+                match self.message {
                     Some(message) => Cow::Borrowed(message),
-                    None => {
-                        if is_empty {
-                            Cow::Borrowed("unexpected end of input")
-                        } else {
-                            Cow::Owned(format!("{}", err.inner().expected()))
-                        }
-                    }
-                },
+                    None => match err.atomic_error() {
+                        Some(message) => Cow::Borrowed(message),
+                        None => Cow::Owned(format!("{}", err.inner().expected())),
+                    },
+                }
             };
 
             err.criticalize(message)
