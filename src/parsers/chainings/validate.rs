fe70b8eb36b750b41d9a5b30aa50eea7367a9181
Clément Nerma <clement.nerma@gmail.com>
2024-05-05 16:29:47 +0200

    Add ability for Validate parsers to be critical

--- src/parsers/chainings/validate.rs
+++ src/parsers/chainings/validate.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{borrow::Cow, marker::PhantomData};
 
 use perfect_derive::perfect_derive;
 
@@ -9,6 +9,7 @@ pub struct Validate<T, P: Parser<T>, F: Fn(&T) -> bool> {
     parser: P,
     validator: F,
     err_msg: Option<&'static str>,
+    critical: bool,
     _t: PhantomData<T>,
 }
 
@@ -18,12 +19,18 @@ impl<T, P: Parser<T>, F: Fn(&T) -> bool> Validate<T, P, F> {
             parser,
             validator,
             err_msg: None,
+            critical: false,
             _t: PhantomData,
         }
     }
 
-    pub fn with_err_msg(mut self, msg: &'static str) -> Self {
+    pub fn with_custom_msg(mut self, msg: &'static str) -> Self {
         self.err_msg = Some(msg);
+        self
+    }
+
+    pub fn as_critical(mut self) -> Self {
+        self.critical = true;
         self
     }
 }
@@ -36,14 +43,18 @@ impl<T, P: Parser<T>, F: Fn(&T) -> bool> Parser<T> for Validate<T, P, F> {
         if (self.validator)(&parsed.data) {
             Ok(parsed)
         } else {
-            // TODO: ranged error (from input start to parsed end)
-            Err(start.custom_err(
-                match &self.err_msg {
-                    Some(msg) => msg,
-                    None => "validator failed",
-                },
-                parsed.at.len,
-            ))
+            let msg = match &self.err_msg {
+                Some(msg) => msg,
+                None => "validator failed",
+            };
+
+            let mut err = start.custom_err(msg, parsed.at.len);
+
+            if self.critical {
+                err = err.criticalize(Cow::Borrowed(msg));
+            }
+
+            Err(err)
         }
     }
 }
